3eca7aaf1a56da9f0c331d13d78a8db65d12922f
David Hunt <davhunt@indiana.edu>
2020-06-05 11:18:47 -0400

    no -applyxfm

--- create_mask.sh
+++ create_mask.sh
@@ -6,6 +6,6 @@ flirt -in T1.nii.gz -ref fMRI_slice.nii.gz -out fs2func.nii.gz -omat fs2func.mat
 
 fslmaths rh.ribbon.nii.gz -add lh.ribbon.nii.gz rh+lh.ribbon.nii.gz # combine rh+lh ribbons
 
-flirt -in rh+lh.ribbon.nii.gz -ref fs2func.nii.gz -out prob_mask.nii.gz -init fs2func.mat -applyxfm # create probabilistic mask
+flirt -in rh+lh.ribbon.nii.gz -ref fs2func.nii.gz -out prob_mask.nii.gz -init fs2func.mat #-applyxfm # create probabilistic mask
 
 fslmaths prob_mask.nii.gz -thr 0.3 -bin mask.nii.gz # threshold to create mask
