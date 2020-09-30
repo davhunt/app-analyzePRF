62db10d1081600dbc48f2bd55444430ecdf6c552
David Hunt <davhunt@indiana.edu>
2020-09-30 11:47:08 -0400

    add comments

--- compile.sh
+++ compile.sh
@@ -8,18 +8,18 @@ echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
 echo "/N/u/brlife/git/mrTools" >> $log
 (cd /N/u/brlife/git/mrTools && git log -1) >> $log
-echo "/N/u/davhunt/Carbonate/analyzePRF/utilities" >> $log
-(cd /N/u/davhunt/Carbonate/analyzePRF/utilities && git log -1) >> $log
-echo "/N/u/davhunt/Carbonate/Downloads/NIfTI_matlab_tools" >> $log
-(cd /N/u/davhunt/Carbonate/NIfTI_cifti_matlab_tools && git log -1) >> $log
+echo "/N/u/brlife/git/NIfTI" >> $log
+(cd /N/u/brlife/git/NIfTI && git log -1) >> $log
+echo "utilities" >> $log
+(cd utilities && git log -1) >> $log
 
 mkdir -p compiled
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/soft/mason/SPM/spm8'))
-addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
-addpath(genpath('/N/u/davhunt/Carbonate/Downloads/NIfTI_matlab_tools'))
+addpath(genpath('/N/u/brlife/git/mrTools'))
+addpath(genpath('/N/u/brlife/git/NIfTI'))
+addpath(genpath('utilities'))
 mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
 exit
 END
--- main.m
+++ main.m
@@ -4,8 +4,8 @@ if ~isdeployed
 	disp('loading paths for IUHPC')
 	addpath(genpath('/N/u/brlife/git/jsonlab'))
 	addpath(genpath('/N/u/brlife/git/mrTools'))
-	addpath(genpath('/N/u/davhunt/Carbonate/analyzePRF/utilities'))
-        addpath(genpath('/N/u/davhunt/Carbonate/Downloads/NIfTI_matlab_tools'))
+	addpath(genpath('/N/u/brlife/git/NIfTI'))
+	addpath(genpath('utilities'))
 end
 
 % load my own config.json
